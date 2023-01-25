5790033c83bc0b59fdb4256ea36476d7fc9c652c
@ingapt <inga.if@gmail.com>
2023-01-25 20:56:43 +0200

    Create Display Menu

--- StudentsIS/Classes/DisplayStart.cs
+++ StudentsIS/Classes/DisplayStart.cs
@@ -1,10 +1,37 @@
-﻿namespace StudentsIS.Classes
+﻿using Microsoft.EntityFrameworkCore;
+using StudentsIS.Classes.Functions;
+
+namespace StudentsIS.Classes
 {
     public static class DisplayStart
     {
-        public static void Start()
-        { 
-        
-        }
+        public static void Start(this StudentContext dbContext)
+        {
+			bool toDo = true;
+
+			while (toDo)
+			{
+				Console.Clear();
+				Console.WriteLine("Pasirinkite: ");
+				Console.WriteLine("[1] Atvaizduoti visus departamento studentus \n[2] Atvaizduoti visas departamento paskaitas \n[3] Atvaizduotis visas paskaitas pagal studentą \n[4] Grįžti atgal");
+				var input = Validation.GetValidNumbersFromConsole(4);
+				switch (input)
+				{
+					case 1:
+						Dispay.DisplayDepartamentsWithStudents(dbContext);
+						break;
+					case 2:
+						Dispay.DisplayDepartamensWithLectures(dbContext);
+						break;
+					case 3:
+						Dispay.DisplayStudentAndYourLectures(dbContext);
+						break;
+					case 4:
+						toDo = false;
+						break;
+					default:
+						break;
+				}
+			}
     }
 }
